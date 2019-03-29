7ce217fb3bd20d9d7a7cc4c1cfff7663c505ea4e
Ricardo Hui <eehaha@gmail.com>
2019-03-29 08:06:12 +0800

    completed

--- SoundShaker/ViewController.swift
+++ SoundShaker/ViewController.swift
@@ -7,14 +7,28 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
-
+    
+    var player = AVAudioPlayer()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if event?.subtype == UIEvent.EventSubtype.motionShake{
+            let soundArray = ["doorbell", "alert"]
+            let randomNumber  = Int(arc4random_uniform(UInt32(soundArray.count)))
+            let fileLocation = Bundle.main.path(forResource: soundArray[randomNumber], ofType:"mp3")
+            do {
+                try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: fileLocation!))
+                
+                player.play()
+            }catch{
+                
+            }
+        }
+    }
 
 }
 
